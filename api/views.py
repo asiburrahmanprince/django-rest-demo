e0c16ca6bb1519c3ab5343217b9b44386108f6e7
asibur-bs <asibur.rahman@brainstation-23.com>
2025-07-24 14:46:39 +0600

    Test function added

--- api/views.py
+++ api/views.py
@@ -4,3 +4,5 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def hello_world(request):
     return Response({"message": "Hello, Git World!"})
+def test_function(request):
+    pass
