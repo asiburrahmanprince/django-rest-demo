da027b4f1795476628fbe1859dcf2b4e886fafbb
asibur-bs <asibur.rahman@brainstation-23.com>
2025-07-25 17:11:19 +0600

    Add Class based filter in sections list page

--- section/views.py
+++ section/views.py
@@ -2,11 +2,25 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Section
 from .forms import SectionForm
+from school_class.models import SchoolClass
 
 class SectionListView(ListView):
     model = Section
     template_name = 'section/section_list.html'
     context_object_name = 'sections'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        class_name = self.request.GET.get('class_name')
+        if class_name:
+            queryset = queryset.filter(school_class__name=class_name)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['classes'] = SchoolClass.objects.all()
+        context['selected_class'] = self.request.GET.get('class_name', None)
+        return context
 
 class SectionCreateView(CreateView):
     model = Section
